66e28c2aaf05941a877502d92672e5a59dbe574d
Krissny <110375406+Krissny@users.noreply.github.com>
2024-09-30 20:59:24 +0530

    Add connectionString

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,18 @@
 const { Pool } = require("pg");
 require('dotenv').config();
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+// // All of the following properties should be read from environment variables
+// // We're hardcoding them here for simplicity
+// module.exports = new Pool({
+//   host: "dpg-crbucdl6l47c73d9do3g-a", // or wherever the db is hosted
+//   user: process.env.USER,
+//   database: "inventory_fxcu",
+//   password: process.env.PASSWORD,
+//   port: 5432 // The default port
+// });
+// const { Pool } = require("pg");
+
+// Again, this should be read from an environment variable
 module.exports = new Pool({
-  host: "dpg-crbucdl6l47c73d9do3g-a", // or wherever the db is hosted
-  user: process.env.USER,
-  database: "inventory_fxcu",
-  password: process.env.PASSWORD,
-  port: 5432 // The default port
+  connectionString: `postgresql://inventory_fxcu_user:QfkSCeGLNQpzmUI0m35ze4t17BCpiOtG@dpg-crbucdl6l47c73d9do3g-a.oregon-postgres.render.com/inventory_fxcu`
 });
