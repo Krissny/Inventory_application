a806bb285ffc6abfcc64de890813e999888a9d19
Krissny <110375406+Krissny@users.noreply.github.com>
2024-10-17 12:07:28 +0530

    Add Data

--- db/populate.js
+++ db/populate.js
@@ -14,11 +14,13 @@ async function insertTable() {
       );
     `);
     await client.query(`
-      DROP TABLE usernames;
-    `);
-    await client.query(`
-      TRUNCATE TABLE Books;
-    `);
+      INSERT INTO Books (Title, Author, Genre, PublicationYear, Link) VALUES
+      ('Lone Wolf and Cub', 'Kazuo Koike', 'Gekiga', 1970, 'https://m.media-amazon.com/images/I/711paX0IgTL._AC_UF1000,1000_QL80_.jpg'),
+      ('The Push Man and Other Stories', 'Yoshihiro Tatsumi', 'Gekiga', 1969, 'https://m.media-amazon.com/images/I/81xA2HfbtWL._AC_UF1000,1000_QL80_.jpg'),
+      ('A Drifting Life', 'Yoshihiro Tatsumi', 'Gekiga', 2008, 'https://upload.wikimedia.org/wikipedia/en/6/6f/A_Drifting_Life.jpg'),
+      ('Red Colored Elegy', 'Seiichi Hayashi', 'Gekiga', 1970, 'https://m.media-amazon.com/images/I/71qgFd+283L._AC_UF1000,1000_QL80_.jpg'),
+      ('Good-Bye', 'Yoshihiro Tatsumi', 'Gekiga', 1971, 'https://goodbye-eri.com/wp-content/uploads/2022/10/img1-2-675x1024.jpg');
+  `);
     console.log("connection successful");
     client.release();
   } catch (err) {
