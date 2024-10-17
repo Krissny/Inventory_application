cc277e6955aefd8b3e203a8744feaf45a1a1c29c
Krissny <110375406+Krissny@users.noreply.github.com>
2024-10-17 12:07:05 +0530

    Change routes to address queries

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const indexRouter = require("./routes/indexRouter");
+
 const path = require("node:path");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", indexRouter);
 
 const port = process.env.PORT || 3000;
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,8 @@
 const { Router } = require("express");
 const indexRouter = Router();
+const indexController = require("../controller/indexController");
 
-indexRouter.get("/");
+indexRouter.get("/", indexController.firstload);
+indexRouter.get("/:Genre", indexController.getShojo);
+
+module.exports = indexRouter;
